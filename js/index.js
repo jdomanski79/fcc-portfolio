6f9ccb4dacb3de5e9a12a8614a567b38ab32ed75
JD <jdomanski79@gmail.com>
2017-11-06 21:54:25 +0100

    ui changes, scroll function aplied

--- js/index.js
+++ js/index.js
@@ -1,22 +1,38 @@
 console.clear();  
-let el = document.getElementById('portfolio');
-  let scrollY = window.pageYOffset;  
-  let scrollDiff = el.offsetTop - scrollY;  
-  console.log(el.offsetTop);
-console.log(window.pageYOffset);
-let y = scrollY;
-window.scrollTo(0,500);
-function scrollTo(){
-    
-    if (window.pageYOffset != el.offsetTop) {
-      y += 10;
-      console.log(y);
-      if (y > el.offsetTop) {
-        y = el.offsetTop;
+const scrollTimes = 40;
+const scrollStepTime = 3;
+
+const navLinks = document.querySelectorAll('nav a');
+
+for (let link of navLinks){
+  link.addEventListener('click', handleScroll);
+}
+
+function handleScroll(event){
+  event.preventDefault();
+  let elId = event.target.href.slice(event.target.href.indexOf('#') + 1);
+  let element = document.getElementById(elId);
+  let navHeight = document.querySelector('nav ul').offsetHeight;
+  const scrollEnd = element.offsetTop - navHeight;
+  const delta = Math.floor((scrollEnd - window.pageYOffset) / scrollTimes);
+  let step = window.pageYOffset;
+  console.log(element, scrollEnd, delta, step,navHeight);
+  scrollToId();
+
+
+
+  function scrollToId(){
+    if (Math.abs(step - scrollEnd) > Math.abs(delta)) {
+      step += delta;
+      // console.log(Math.abs(step - scrollEnd), delta);
+    }
+    else {
+      step = scrollEnd;
+      window.scrollTo(0,step);
+        
         return;
-      }
-      window.scrollTo(0,y);
-      setTimeout(scrollTo,100);
-    };
+    }
+    window.scrollTo(0,step);
+    setTimeout(scrollToId, scrollStepTime);
   }
-scrollTo();
+}
